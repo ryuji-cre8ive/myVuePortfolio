64769fc79daa8875c14bd98a8e9b8a50cd9b4b17
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2022-11-08 17:15:09 +0900

    Netlifyにdeployするにあたり改変しました

---
+++ components/Modal6.vue
@@ -0,0 +1,26 @@
+// Modal.vue 子コンポーネント
+<template>
+  <div class="modal">
+    <div class="bg" @click="$emit('closeModal')">
+      <div class="modal-wrap">
+        <div class="content">
+          <div class="text">
+            <h1>Randoom</h1>
+            <p>
+              現在地から半径500m圏内のカフェやレストランをランダムに決めてくれるWebApp。飲食店にはいきたいがなんでもいいという時に使ってほしい。
+            </p>
+          </div>
+          <button @click="$emit('closeModal')">
+            <a class="btn btn-border" target="_blank" href="https://random-food-338110.an.r.appspot.com">Link</a>
+          </button>
+        </div>
+     <!-- このボタンを押すとカスタムイベントcloseModalが発生 -->
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss">
+  /*その他と主な共通部分は省略*/
+
+  
+</style>
---
+++ components/Work5.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="square">
+    <p>・</p>
+    <img src="../static/img/randoom.png" alt="Randoom" height="200">
+    <button class="btn btn-border" v-on:click="show=true">Randoom</button>
+    <Modal6 @closeModal="show=false" v-if="show"/>
+  </div>
+</template>
+
+<script>
+
+import Modal6 from '~/components/Modal6.vue'
+
+export default {
+  name: 'work5',
+  components: {
+    Modal6
+  },
+  data(){
+    return{
+      show: false
+    }
+  },
+
+}
+</script>
